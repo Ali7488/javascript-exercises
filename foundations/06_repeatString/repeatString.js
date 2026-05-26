be0906e70cd7e33874c86ee93ebe3a71802f9c75
Ali7488 <mohannad.eali@gmail.com>
2026-05-25 19:39:47 -0600

    done exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, numOfReps) {
+  if (numOfReps < 0) {
+    return "ERROR";
+  }
+  let returnedString = "";
+  for (let i = 0; i < numOfReps; i++) {
+    returnedString = returnedString + str;
+  }
+  return returnedString;
 };
 
 // Do not edit below this line
